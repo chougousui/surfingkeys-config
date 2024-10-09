c396c39faac638cd7f6592ff4a9f866b200ddf54
choug <chougousui@gmail.com>
2024-10-09 23:32:17 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+## SurfingKeys config
+
+[cvim](https://github.com/1995eaton/chromium-vim)已经停止维护.
+
+`vimium`等功能极差,比如不能在搜索框中按`<Ctrl-e>`跳到行尾.
+
+还好发现了 [SurfingKeys](https://github.com/brookhong/Surfingkeys/tree/master?tab=readme-ov-file#edit-your-own-settings)
+
+## Features
+
+面向`workman`键盘布局做一些优化
+
+1. `r`向上滚动半屏
+2. `h`向下滚动半屏
+3. `R`刷新页面
+
+面向原先`cvim`习惯的修改
+
+1. 关闭smoothscroll
+2. hint显示在左侧而不是中间
+3. 使用`F`表示在后台打开新标签页,同时`gf`为相同含义
+
+## Usage
+
+1. 插件的配置中开启 `Advanced mode`
+
+2. 复制`config.js`文件的raw链接
---
+++ config.js
@@ -0,0 +1,118 @@
+// example setting https://github.com/Foldex/surfingkeys-config/blob/master/config.js
+const { unmapAllExcept, mapkey, Normal, RUNTIME, Hints, map } = api;
+
+// https://github.com/brookhong/Surfingkeys/tree/master?tab=readme-ov-file#edit-your-own-settings
+settings.smoothScroll = false;
+settings.hintAlign = "left";
+
+// TODO why not working?
+// Surfingkeys/src/content_scripts/common/runtime.js
+// settings.nextLinkRegex = /(\b(next)\b)|下页|下一页|后页|下頁|下一頁|後頁|>>|»/i;
+// settings.prevLinkRegex = /(\b(prev|previous)\b)|上页|上一页|前页|上頁|上一頁|前頁|<<|«/i;
+
+// Surfingkeys/docs/API.md
+unmapAllExcept([
+  // 帮助
+  "?",
+  ";ql",
+
+  // 鼠标点击
+  "gi",
+  "[[",
+  "]]",
+  "f",
+  "af",
+  "gf",
+  "q",
+
+  // 滚动
+  "gg",
+  "G",
+
+  // 标签页
+  "yt",
+  "yT",
+  "gxx",
+  "T",
+  "<Alt-p>",
+  "<Alt-m>",
+  "x",
+  "X",
+
+  // 缩放
+  "zr",
+  "zi",
+  "zo",
+
+  // 网页浏览
+  "gU",
+  "S",
+  "D",
+
+  // 剪贴板
+  "yG",
+  "yS",
+  "yg",
+  "yy",
+  "yY",
+  "yl",
+  "yf",
+  "yp",
+
+  // 搜索栏
+  "om",
+  ":",
+  "<Esc>",
+  "<Ctrl-'>",
+  "<ArrowDown>",
+  "<ArrowUp>",
+  "<Ctrl-n>",
+  "<Ctrl-p>",
+
+  // 可视模式
+  "/",
+  "n",
+  "N",
+
+  // 设置
+  ";pm",
+  ";e",
+  ";v",
+
+  // Chrome内置功能
+  "gd",
+  "ge",
+  ";i",
+
+  // 插入模式
+  "<Ctrl-e>",
+  "<Ctrl-f>",
+  "<Ctrl-u>",
+  "<Alt-b>",
+  "<Alt-f>",
+  "<Alt-w>",
+  "<Alt-d>",
+  "<Esc>",
+  "<Ctrl-'>",
+  "<Ctrl-i>",
+  "<Ctrl-Alt-i>",
+]);
+
+mapkey("r", "Scroll up", () => {
+  Normal.scroll("pageUp");
+});
+
+mapkey("h", "Scroll down", () => {
+  Normal.scroll("pageDown");
+});
+
+// 1. "刷新当前页"
+// 2. Surfingkeys/src/pages/l10n.json
+// 3. "Reload the page"
+// 4. Surfingkeys/src/content_scripts/common/default.js
+// 5. RUNTIME("reloadTab", { nocache: false });
+mapkey("R", "Reload the page", () => {
+  RUNTIME("reloadTab", { nocache: false });
+});
+
+map("F", "gf");
